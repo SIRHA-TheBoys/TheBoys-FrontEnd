5834b54c59276b72c5d46b1166b1248196df4468
Julian <julianl116910@gmail.com>
2025-10-26 18:34:25 -0500

    Requests Conectadas backy

--- src/pages/Request/Request.tsx
+++ src/pages/Request/Request.tsx
@@ -1,16 +1,19 @@
+import "./Request.css";
 import SideBar from "../../components/SideBar/SideBar";
 import TopBar from "../../components/TopBar/TopBar";
 import PopUpRequest from "../../components/PopUpRequest/PopUpRequest";
-import "./Request.css";
+import RequestTable from "../../components/RequestTable/RequestTable";
 
 export default function Request() {
   return (
   <div>
     <TopBar />
     <SideBar />
+
     <div className="request">
       <h3 className="request-label">Schedule Change Requests</h3>
       <h1 className="request-welcome">Consult and create your requests</h1>
+      <RequestTable />
       <PopUpRequest />
     </div>
   </div>
