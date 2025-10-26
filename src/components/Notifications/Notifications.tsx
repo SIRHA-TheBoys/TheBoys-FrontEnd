75ecaba8adc79371fa834e4cadaddc256c58f241
Daniel <daniel.patino-m@mail.escuelaing.edu.co>
2025-10-26 01:34:14 -0500

    add: home screen

--- src/components/Notifications/Notifications.tsx
+++ src/components/Notifications/Notifications.tsx
@@ -1,9 +1,14 @@
 import "./Notifications.css";
+import { IoIosWarning } from "react-icons/io";
 
 export default function Notifications() {
   return (
     <div className="notifications">
-      <h1>Hola!</h1>
+      <IoIosWarning className="wIcon"/>
+      <div className="text">
+        <h2>Pending to approve</h2>
+        <h3>lorem ipsum</h3>
+      </div>
     </div>
   );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,57 @@
 import Notifications from "../../components/Notifications/Notifications";
 import "./Home.css";
-import SideBar from "../../components/SideBar/SideBar";
 import TopBar from "../../components/TopBar/TopBar";
+import {FaUser, FaCalendarAlt, FaExchangeAlt, FaTrafficLight} from "react-icons/fa";
+import { Link } from "react-router-dom"
+
 export default function Home() {
   return (
     <div>
       <TopBar />
-      <SideBar />
-      <div className="home">
-        <Notifications />
+      <div className = "mainContainer">
+        <div className = "buttonsContainer">
+          <nav>
+            <ul>
+              <li>
+                <Link to = "/schedule" className = "home-links">
+                  <FaCalendarAlt className="icon"/>
+                  <span> My Schedule </span>
+                </Link>
+              </li>
+              <li>
+                <Link to = "/academicTrafficLight" className="home-links">
+                  <FaTrafficLight className="icon"/>
+                  <span> Academic traffic light</span>
+                </Link>
+              </li>
+              <li>
+                <Link to = "/requests" className = "home-links">
+                  <FaExchangeAlt className="icon"/>
+                  <span>My requests</span>
+                </Link>
+              </li>
+              <li>
+                <Link to = "/basicInformation" className = "home-links">
+                  <FaUser className="icon"/>
+                  <span>Basic information</span>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="notificationContainer">
+          <h2>Notifications</h2>
+          <Notifications/>
+          <nav>
+            <ul>
+              <li>
+                <Link to = "/requests" className= "notifications-link">
+                  <span>View all notifications</span>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </div>
   );
