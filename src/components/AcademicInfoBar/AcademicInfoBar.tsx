35256a90e1b59e13fb9ed4b7c15c811c9565a6a9
sebasPuentes <seb123.juli@gmail.com>
2025-10-26 02:33:08 -0500

    add: conexion plan de estudios

--- src/components/AcademicInfoBar/AcademicInfoBar.tsx
+++ src/components/AcademicInfoBar/AcademicInfoBar.tsx
@@ -5,31 +5,30 @@ export default function AcademicInfoBar(user : User) {
   return (
     <div className="student-info-card">
       <div className="student-info-header">
-        <h2>{user?.career}</h2>
-        <p>PLAN 15</p>
+        <h2>PLAN 15</h2>
+        <h2>{user?.faculty}</h2>
       </div>
 
       <div className="student-info-details">
         <div>
-          <p><strong>ID:</strong> {user?.name}</p>
-          <p><strong>CC:</strong> {user?.name}</p>
-          <p><strong>Grado:</strong> {user?.name}</p>
-          <p><strong>Plan Académico:</strong> {user?.name}</p>
-          <p><strong>Programa Académico:</strong> {user?.name}</p>
+          <p><strong>ID:</strong> {user?.id}</p>
+          <p><strong>Grado:</strong> Pregrado</p>
+          <p><strong>Plan Académico:</strong> 15</p>
+          <p><strong>Programa Académico:</strong> {user?.career}</p>
         </div>
 
         <div>
-          <p><strong>Estado:</strong> {user?.name}</p>
-          <p><strong>Promedio acum:</strong> {user?.name}</p>
-          <p><strong>Promedio de grado:</strong> {user?.career}</p>
-          <p><strong>Situación:</strong> {user?.career}</p>
+          <p><strong>Estado:</strong> Activo </p>
+          <p><strong>Promedio acum:</strong> {user.studyPlan?.average}</p>
+          <p><strong>Promedio de grado:</strong> {user.studyPlan?.average}</p>
+          <p><strong>Situación:</strong> Regular</p>
         </div>
 
         <div>
-          <p><strong>Créditos Plan:</strong> {user?.career}</p>
-          <p><strong>Aprobados:</strong> {user?.career}</p>
-          <p><strong>Semestre a Cursar:</strong> {user?.career}</p>
-          <p><strong>Correo-Campus:</strong> {user?.career}</p>
+          <p><strong>Créditos Plan:</strong> 139</p>
+          <p><strong>Aprobados:</strong> FFF</p>
+          <p><strong>Semestre Cursando:</strong> {user?.semester}</p>
+          <p><strong>Correo-Campus:</strong> {user?.email}</p>
         </div>
       </div>
     </div>
