06f5ac5d3c4bb71b09de9c41f31e2468edeb1f40
Daniel <daniel.patino-m@mail.escuelaing.edu.co>
2025-10-26 01:37:58 -0500

    add: home screen

--- src/hooks/userHook.ts
+++ src/hooks/userHook.ts
@@ -1,22 +1,23 @@
-import { useState, useEffect} from "react";
-import {User} from "../types/user"
+import { useState, useEffect } from "react";
+import { User } from "../types/user"
 
-export function userHook(){
+export function userHook() {
 
-    const [user, setUser] = useState<User | null >(null);
-    useEffect(() => {
-            const storedUser = localStorage.getItem("user");
-            if (!storedUser) return;
-            const userI = JSON.parse(storedUser);
-            console.log(userI)
-            fetch(`http://localhost:8080/users/${userI.id}`)
-                .then(res => res.json())
-                .then(response => {
-                    const user  = response
-                    console.log (response)
-              setUser(user)
-            });
-          
-          },[])
-    return { user };
+  const [user, setUser] = useState<User | null>(null);
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (!storedUser) return;
+    const userI = JSON.parse(storedUser);
+    const roleU = userI.role.toLowerCase();
+    console.log(userI)
+    fetch(`http://localhost:8080/${roleU}/${userI.id}`)
+      .then(res => res.json())
+      .then(response => {
+        const user = response
+        console.log(response)
+        setUser(user)
+      });
+
+  }, [])
+  return { user };
 }
